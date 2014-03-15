bd179fb1e3d5dcba89a4efc82227684b29dbb917
mktitan <mk_titan@hotmail.com>
2014-03-15 18:01:59 +0100

    Initial AuthService with hardcoded replies

---
+++ Program/ShareIt/ShareIt/ShareItServices/AuthService.cs
@@ -0,0 +1,15 @@
+﻿namespace ShareItServices
+{
+    public class AuthService: IAuthService
+    {
+        public string GetAuthToken(string username, string password)
+        {
+            return "testAuthToken";
+        }
+
+        public bool Authenticate(string token)
+        {
+            return token == "testAuthToken";
+        }
+    }
+}
---
+++ Program/ShareIt/ShareIt/ShareItServices/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+
+namespace ShareItServices
+{
+    [ServiceContract]
+    public interface IAuthService
+    {
+        [OperationContract]
+        string GetAuthToken(string username, string password);
+
+        [OperationContract]
+        bool Authenticate(string token);
+
+    }
+}
