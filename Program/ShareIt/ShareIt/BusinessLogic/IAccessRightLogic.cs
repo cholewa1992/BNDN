3fab20e8ddb26cb53c6cd36737c65f2cdedccd59
Crosan <cough12@hotmail.com>
2014-03-16 17:31:21 +0100

    Finished AccessRight service

--- Program/ShareIt/ShareIt/BusinessLogic/IAccessRightLogic.cs
+++ Program/ShareIt/ShareIt/BusinessLogic/IAccessRightLogic.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogicLayer.DTO;
 
 namespace BusinessLogicLayer
 {
     public interface IAccessRightLogic
     {
+        bool Purchase(User u, MediaItem m, Client c, DateTime expiration);
+        bool Upload(User u, MediaItem m, Client c);
+        bool MakeAdmin(User newAdmin);
+        bool DeleteAccessRight(AccessRight ar);
+        List<AccessRight> GetAccessRightInfo(User u);
+        bool EditExpiration(AccessRight oldAR, AccessRight newAR);
     }
 }
