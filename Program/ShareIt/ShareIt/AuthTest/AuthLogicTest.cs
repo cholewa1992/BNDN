51f0625db9a74584ecd1a6d7a35d15e4b5e05ffa
mktitan <mk_titan@hotmail.com>
2014-03-16 17:49:08 +0100

    AuthTest

---
+++ Program/ShareIt/ShareIt/AuthTest/AuthLogicTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BusinessLogicLayer;
+using BusinessLogicLayer.DTO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AuthTest
+{
+    [TestClass]
+    public class AuthLogicTest: BaseTest
+    {
+
+        AuthLogic al = new AuthLogic();
+
+        [TestMethod]
+        public void TestNoUserNamePrecondition()
+        {
+            Throws<ArgumentException>(() => 
+                al.CheckUserExists(new User() { Password = "testpass" }),
+                @"Precondition failed: !string.IsNullOrEmpty(user.Username)"
+                );
+        }
+
+        [TestMethod]
+        public void TestNoUserPasswordPrecondition()
+        {
+            Throws<ArgumentException>(() =>
+                al.CheckUserExists(new User() { Username = "John"}),
+                @"Precondition failed: !string.IsNullOrEmpty(user.Password)"
+                );
+        }
+    }
+}
