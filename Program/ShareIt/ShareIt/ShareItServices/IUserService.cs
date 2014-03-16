c4bcf1721cccd358ddf69c8de32df0c164b8b474
nicki-alia <nicki@alia.dk>
2014-03-16 16:03:18 +0100

    UserService reference fix

--- Program/ShareIt/ShareIt/ShareItServices/IUserService.cs
+++ Program/ShareIt/ShareIt/ShareItServices/IUserService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
-using ShareItServices.DataContracts;
+using BusinessLogicLayer.DTO;
 
 namespace ShareItServices
 {
