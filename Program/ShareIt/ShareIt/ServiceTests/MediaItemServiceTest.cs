6b1bee4c9ae6adb76528affa329a5063103a2c4f
Thomas Dragsbæk <thst@itu.dk>
2014-03-23 11:59:27 +0100

    Moved MediaItemLogicTest and MediaItemServiceTest

---
+++ Program/ShareIt/ShareIt/ServiceTests/MediaItemServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using BusinessLogicLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShareIt;
+
+namespace ServiceTests
+{
+    [TestClass]
+    public class MediaItemServiceTest
+    {
+        private MediaItemService _mediaItemService = new MediaItemService(BusinessLogicFacade.GetTestFactory());
+
+        [TestMethod]
+        public void TestGetMediaItemValidId_FileExtension()
+        {
+            var mediaItem = _mediaItemService.GetMediaItemInformation(1, "token");
+            Assert.AreEqual(".pdf", mediaItem.FileExtension);
+        }
+
+        [TestMethod]
+        public void TestGetMediaItemValidId_InformationCount()
+        {
+            var mediaItem = _mediaItemService.GetMediaItemInformation(1, "token");
+            Assert.AreEqual(5, mediaItem.Information.Count());
+        }
+
+        [TestMethod]
+        public void TestGetMediaItemInvalidId()
+        {
+            try
+            {
+                _mediaItemService.GetMediaItemInformation(999, "token");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("No media item with id 999 exists in the database", e.Message);
+            }
+            Assert.Fail("Expected ArgumentException");
+        }
+    }
+}
