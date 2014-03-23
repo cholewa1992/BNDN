655734c990f6a698675feecebbc7bd412faaeb74
mktitan <mk_titan@hotmail.com>
2014-03-23 15:43:30 +0100

    AuthTest

---
+++ Program/ShareIt/ShareIt/BusinessLogicTests/AuthLogicTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BusinessLayerTest;
+using BusinessLogicLayer;
+using DataAccessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogicTests
+{
+    [TestClass]
+    public class AuthLogicTest: BaseTest
+    {
+
+        IAuthInternalLogic al = new AuthLogic(new StorageBridge(new EfStorageConnection<BNDNEntities>()));
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(-1, al.CheckClientToken("testToken"));
+        }
+
+        [TestMethod]
+        public void EmptyClientToken()
+        {
+            Throws<Argu>(() => al.CheckClientToken(""));
+            Assert.AreEqual(-1, al.CheckClientToken("testToken"));
+        }
+    }
+}
