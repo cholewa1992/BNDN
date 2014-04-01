4a0c185ab5333dd62ad1990242ee357e89f071b6
mktitan <mk_titan@hotmail.com>
2014-04-01 16:59:33 +0200

    AuthLogic dunno changes lol

--- Program/ShareIt/ShareIt/BusinessLogic/AuthLogic.cs
+++ Program/ShareIt/ShareIt/BusinessLogic/AuthLogic.cs
@@ -35,6 +35,7 @@ namespace BusinessLogicLayer
             Contract.Requires<ArgumentException>(userId > 0);
             Contract.Requires<ArgumentException>(mediaItemId > 0);
 
+
             // TODO use .single instead of .First
 
             //Find an accessright
@@ -201,7 +202,7 @@ namespace BusinessLogicLayer
             throw new InstanceNotFoundException("No expiration date was found");
         }
 
-
+        
 
         public void Dispose()
         {
