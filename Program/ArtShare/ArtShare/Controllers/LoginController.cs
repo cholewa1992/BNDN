7a0ffb751c2a167b6c42b1ac7c7ddbc1b217124f
Jacob Cholewa <jacob@cholewa.dk>
2014-04-07 22:34:12 +0200

    Added LoginController

---
+++ Program/ArtShare/ArtShare/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using ArtShare.Models;
+using ShareItServices.AuthService;
+
+namespace ArtShare.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            //Creates a new model
+            var model = new LoginModel();
+
+            //Opens a AuthServiceClient
+            using (var client = new AuthServiceClient())
+            {
+                //Creating user object
+                model.User = new UserDTO {Username = username, Password = password};
+                //Validating user
+                model.User.Id = client.ValidateUser(model.User, Properties.Resources.ClientToken);
+                //If the user was validated, set LoggedIn property to true
+                model.LoggedIn = model.User.Id > 0;
+            }
+
+            var userCookie = new HttpCookie("user", model.User.Id + "");
+            userCookie.Expires.AddDays(365);
+            HttpContext.Response.Cookies.Add(userCookie);
+
+
+
+            return Index();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            if (Request.Cookies["user"] != null)
+            {
+                var user = new HttpCookie("user")
+                {
+                    Expires = DateTime.Now.AddDays(-1),
+                    Value = null
+                };
+                Response.Cookies.Add(user);
+            }
+            return Index();
+        }
+    }
+}
---
+++ Program/ArtShare/ArtShare/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ShareItServices;
+using ShareItServices.AuthService;
+
+namespace ArtShare.Models
+{
+    public class LoginModel
+    {
+        [Display(Name = "User")]
+        public UserDTO User { get; set; }
+
+        [Display(Name = "Login status")]
+        public bool LoggedIn { get; set; }
+    }
+}
