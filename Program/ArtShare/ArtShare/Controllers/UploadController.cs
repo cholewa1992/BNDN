c2807f6d5df9855dce797a10862064edeff06843
9cki <nhjo@itu.dk>
2014-04-25 22:29:25 +0200

    UploadModel + Initial working view

--- Program/ArtShare/ArtShare/Controllers/UploadController.cs
+++ Program/ArtShare/ArtShare/Controllers/UploadController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ArtShare.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,6 +16,25 @@ namespace ArtShare.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(UploadModel um)
+        {
+            if (ModelState.IsValid)
+            {
+                // Use your file here
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    um.File.InputStream.CopyTo(memoryStream);
+
+                    TempData["success"] = "LEL" + memoryStream.Length.ToString();
+                }
+            }
+
+            
+
+            return RedirectToAction("Index", "Home");
         }
 
     }
---
+++ Program/ArtShare/ArtShare/Models/UploadModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace ArtShare.Models
+{
+    public class UploadModel
+    {
+        public HttpPostedFileBase File { get; set; }
+
+
+    }
+}
