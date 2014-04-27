d18b0137520144608877a84ba26ff30f0d0372a8
9cki <nhjo@itu.dk>
2014-04-27 13:17:08 +0200

    Added FeaturedModel

---
+++ Program/ArtShare/ArtShare/Models/FeaturedModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtShare.Models
+{
+    public class FeaturedModel
+    {
+        public List<MovieDetailsModel> Movies { get; set; }
+
+        public List<MusicDetailsModel> Music { get; set; }
+
+        public List<BookDetailsModel> Books { get; set; }
+    }
+}
